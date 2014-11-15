d4a1b3240137bdfa454f20d6f49c3bca1b550e07
Aaron France <aaron.l.france@gmail.com>
2014-11-15 20:41:59 +0100

    Revert "update dict/0 to dict:dict/0 for R17 support"
    This reverts commit 56ebf0d6f1aaab31c7d0f25d17ad4383f7c9051c.

--- include/elasticsearch_types.hrl
+++ include/elasticsearch_types.hrl
@@ -54,14 +54,14 @@
 
 -record(restRequest, {method                    :: integer(),
                       uri                       :: string() | binary(),
-                      parameters = dict:new()   :: dict:dict(),
-                      headers = dict:new()      :: dict:dict(),
+                      parameters = dict:new()   :: dict(),
+                      headers = dict:new()      :: dict(),
                       body = <<"">>             :: string() | binary()
                      }).
 
 %% struct restResponse
 
 -record(restResponse, {status :: integer(),
-                       headers :: dict:dict(),
+                       headers :: dict(),
                        body :: undefined | string() | binary()
                       }).
